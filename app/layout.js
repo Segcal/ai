ce7a3062d5ec66a5b76caad8eee60f2947c3fbad
Segcal <seguncaleb3@gmail.com>
2023-10-23 15:02:17 +0100

    main

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const poppins = Poppins({ subsets: ['latin'], weight: ['200', '400', '600', '800'] })
 
 export const metadata = {
-  title: 'Unriddle | AI',
+  title: '| AI',
   description: 'Next generation AI',
 }
 
